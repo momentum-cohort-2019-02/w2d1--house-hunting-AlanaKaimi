0c9e96b37e4bfdc89dbba7461a0e9317cfd85150
AlanaKaimi <ajbryce@live.com>
2019-02-19 13:29:43 -0500

    Needed tons of help on this one. Thanks Anna and Jaun for all the extra support!

--- house_hunt.py
+++ house_hunt.py
@@ -1,37 +1,28 @@
-print("How long will it take me to buy a house?")
-r = 0.04
+# How long will it take to save enough for Down Payment on House?
+
+# Get annual salary
+annual_salary = float(input("Enter your annual salary: $"))
+# Get % of monthly salary to be saved
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+# Get cost of dream home
+total_cost = float(input("Enter the cost of your dream home: "))
+# Calculate Down payment
+portion_down_payment = total_cost * 0.25
+
+# Define variables
 current_savings = 0
+r = 0.04
 number_of_months = 0
-months_in_year = 12
 
+# Each month
+while current_savings <= portion_down_payment:
+    # add month
+    number_of_months += 1
+    # calculate new current savings
+    current_savings += (annual_salary/12)*portion_saved + current_savings*(r/12)
 
-annual_salary = input("Enter your annual salary: ")
-portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
-
-
-
-total_cost = input("Enter the cost of your dream home: ")
-portion_down_payment = int(total_cost) * float(0.25)
-print("Your Down payment will be " + str(portion_down_payment) + ".")
-
-# Repeat each month:
-
-save_more = True
-while save_more:
-    monthly_savings = annual_salary // str(12) * str(portion_saved) 
-    ann_return_month = current_savings * r / 12
-    current_savings = monthly_savings + ann_return_month
-
-    if current_savings >= portion_down_payment:
-        save_more = False
-    else:
-        number_of_months = number_of_months + 1
-
-print("Congrats! you saved " +str(current_savings) + "in just" + str(num_of_months) + "! enough for that " +str(portion_down_payment) + " down payment!")
-
-
-
-# print("Number of months:")
+# return answer
+print("Number of Months ", number_of_months)
 
 
 
